a7bc04c29aa4c16c9bdc8c377a8bda0480a0021c
Williams Junnior <wjunniorrojasflores@gmail.com>
2025-02-09 04:51:21 -0500

    Proyecto-challenge

---
+++ app.js
@@ -0,0 +1,46 @@
+let grupoAmigos = [];
+
+
+function cambiarTexto(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+  return;
+}
+
+function agregarAmigo() {
+  amigos = document.getElementById("amigo").value;
+  if (amigos == "") {
+    alert("Ingrese un valor valido");
+
+    if (grupoAmigos.length == amigos) {
+      alert("El amigo ya esta en la lista");
+    }
+  } else {
+    if (grupoAmigos.includes(amigos)) {
+      alert("El amigo ya esta en la lista");
+    } else if (grupoAmigos.length >= 5) {
+      alert("El maximo de amigos es 5");
+      console.log(grupoAmigos);
+    } else {
+      grupoAmigos.push(amigos);
+      document.getElementById("amigo").value = "";
+      mostrarAmigos();
+    }
+  }
+}
+
+function mostrarAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+  for (let i = 0; i < grupoAmigos.length; i++) {
+    listaAmigos.innerHTML += `<li>${grupoAmigos[i]}</li>`;
+  }
+
+}
+
+function sortearAmigo() {
+  let amigoSorteado = Math.floor(Math.random() * grupoAmigos.length);
+  amigoSorteado = grupoAmigos[amigoSorteado];
+  document.getElementById("resultado").innerHTML = `El amigo secreto es: ${amigoSorteado}`;
+  return;
+}
